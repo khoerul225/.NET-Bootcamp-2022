11a3e9dbd9077af06fe13077dc3df5428d70d4a1
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-10 13:12:24 +0700

    /Code Practice

--- NorthwindSolution/Northwind.Web/Models/Employee.cs
+++ NorthwindSolution/Northwind.Web/Models/Employee.cs
@@ -6,7 +6,6 @@ namespace Northwind.Web.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-            
         public DateTime BirthDate { get; set; }
     }
 }

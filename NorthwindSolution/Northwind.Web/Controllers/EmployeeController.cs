c59501649810207f5761782b3b56f854c3116bdf
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-10 13:09:26 +0700

    //Code Practice

---
+++ NorthwindSolution/Northwind.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Web.Models;
+using System.Collections.Generic;
+
+
+namespace Northwind.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult ListEmployee()
+        {
+            //Code Practice
+            var listOfEmployee = new List<Employee>() {
+                new Employee {Id=1001, Name="Bang Taqin", BirthDate =new DateTime (1999,07,03) },
+                new Employee {Id=1002, Name="Ka Silvi", BirthDate =new DateTime (2002,04,24) },
+                new Employee {Id=1003, Name="Ka Intan", BirthDate =new DateTime (2002,08,11)}
+
+            };
+            return View("ListEmployee", listOfEmployee);
+        }
+    }
+}
---
+++ NorthwindSolution/Northwind.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Northwind.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var myName = "Kang Dian";
+            return View("Index",myName);
+        }
+
+        public IActionResult Privacy()
+        {
+            var hour = DateTime.Now.Hour;
+            //Tennary Operation (If/Else)
+            var waktu = hour > 12 ? "Good Day" : "Good Mornig";
+            return View("privacy",waktu);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

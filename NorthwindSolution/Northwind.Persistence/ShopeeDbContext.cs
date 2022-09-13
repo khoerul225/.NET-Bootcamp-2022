8970ce60108e6725a9d7ea8160b8685845c6ab5a
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-13 11:09:35 +0700

    Project Reference

---
+++ NorthwindSolution/Northwind.Persistence/ShopeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using N
+
+namespace Northwind.Persistence
+{
+    public class ShopeeDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptions options;
+
+        public ShopeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- NorthwindSolution/Northwind.Web/Repository/EmployeeRepository.cs
+++ NorthwindSolution/Northwind.Web/Repository/EmployeeRepository.cs
@@ -18,7 +18,7 @@ namespace Northwind.Web.Repository
             {
                 new Employee {Id=1001, Name="Ahsan Murobi", Email="Ahsanmurobi@gmail.com", BirthDate =new System.DateTime (1996,04,24) },
                 new Employee {Id=1002, Name="Khoerul Mutaqin",Email="Khoerulmutaqin@gmail.com", BirthDate =new DateTime (1999,07,03) },
-                new Employee {Id=1003, Name="Silvia Raudha",Email="Silviaraudha@gmail.com", BirthDate =new DateTime (2002,03,24)},
+/*                new Employee {Id=1003, Name="Silvia Raudha",Email="Silviaraudha@gmail.com", BirthDate =new DateTime (2002,03,24)},*/
                 new Employee {Id=1004, Name="Apnan Faiz",Email="Apnanfaiz@gmail.com", BirthDate =new DateTime (2003,07,22)},
                 new Employee {Id=1005, Name="Ahmad Abdul Muiz",Email="Ahmadmuiz@gmail.com", BirthDate =new DateTime (2011,07,11)},
 

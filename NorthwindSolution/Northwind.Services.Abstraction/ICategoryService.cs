c14a5f803d46ac542b227d7e014826abd7b80934
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-19 10:25:06 +0700

    I Service Manager

---
+++ NorthwindSolution/Northwind.Services.Abstraction/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Northwind.Contracts.Dto.Category;
+
+
+namespace Northwind.Services.Abstraction
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDto>> GetAllCategory(bool trackChanges);
+
+        Task<CategoryDto> GetCategoryById(string customerId, bool trackChanges);
+
+        void Insert(CategoryForCreateDto categoryForCreateDto);
+
+        void Edit(CategoryDto categoryDto);
+
+        void Remove(CategoryDto categoryDto);
+    }
+}
---
+++ NorthwindSolution/Northwind.Services/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Northwind.Contracts.Dto.Category;
+using Northwind.Domain.Base;
+using Northwind.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Northwind.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        //2 private dibawah kemudian di block kemudian ctrl + . (dot) generate constructor
+        private IRepositoryManager _repositoryManager;
+        private readonly IMapper _mapper;
+        
+        //defedency injection
+        public CategoryService(IRepositoryManager repositoryManager, IMapper mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _mapper = mapper;
+        }
+
+        public void Edit(CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetAllCategory(bool trackChanges)
+        {
+            var categoryModel = await _repositoryManager.CategoryRepository.GetAllCategory(trackChanges);
+            var categoryDto = _mapper.Map<IEnumerable<CategoryDto>>(categoryModel);
+            return categoryDto;
+        }
+
+        public Task<CategoryDto> GetCategoryById(string customerId, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(CategoryForCreateDto categoryForCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
